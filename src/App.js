19021132cac8b54c1a3d33d62021dea6703738f0
CGA <59731043+mohanvej@users.noreply.github.com>
2021-02-08 16:30:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Code from "./Code";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Code name="Guruva" />
+      <Code name="Code" />
+    </div>
+  );
+}
---
+++ src/Code.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Code = (props) => {
+  console.log(props);
+  return <h1>Welcome to Code{props.name}</h1>;
+};
+
+export default Code;
